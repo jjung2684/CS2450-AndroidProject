f7d1ceb248300b5df9634eb31d9cae695c5d83a3
Spencer Barrett <srbarrett.cpp.edu>
2022-12-04 21:41:13 -0800

    Basic functionality

--- app/src/main/java/com/android/cs2450_androidproject/GameActivity.java
+++ app/src/main/java/com/android/cs2450_androidproject/GameActivity.java
@@ -1,27 +1,36 @@
 package com.android.cs2450_androidproject;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
-import android.content.Context;
+import android.annotation.SuppressLint;
+
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Gravity;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-public class GameActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class GameActivity extends AppCompatActivity{
 int numCards;
+int score;
+    Button try_again;
     MediaPlayer mediaPlayer;
     TextView score_box;
     LinearLayout main_Layout;
+    String[] gameWords = {"Panda", "Lion", "Tiger", "Bear", "Eagle", "Snake", "Cheetah",
+            "Jaguar", "Dolphin", "Wolf"};
+    ArrayList<String> open_List;
+    ArrayList<Button> bttn_List;
+    ArrayList<Button> rmv_List;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -32,10 +41,16 @@ int numCards;
         Log.d("num", String.valueOf(cards));
 
 
+
+
         mediaPlayer = MediaPlayer.create(GameActivity.this, R.raw.jazz_music);
         Intent music = new Intent(GameActivity.this, BackgroundSoundService.class);
         startService(music);
         main_Layout = (LinearLayout) findViewById(R.id.mainLayout);
+        open_List= new ArrayList<>();
+        bttn_List = new ArrayList<>();
+        rmv_List = new ArrayList<>();
+
 
         switch(cards){
             case 4:
@@ -43,10 +58,121 @@ int numCards;
                 LayoutInflater inflater = LayoutInflater.from(GameActivity.this);
                 View inflatedLayout = inflater.inflate(R.layout.level_4_layout, null);
                 setContentView(inflatedLayout);
+                score_box = (TextView) findViewById(R.id.score_box2);
+                score_box.setText("Score: 0");
+                try_again = (Button) findViewById(R.id.try_again);
+
+
+                @SuppressLint({"MissingInflatedId", "LocalSuppress"}) Button c1 = (Button) findViewById(R.id.card1);
+                @SuppressLint({"MissingInflatedId", "LocalSuppress"}) Button c2 = (Button) findViewById(R.id.card2);
+                @SuppressLint({"MissingInflatedId", "LocalSuppress"}) Button c3 = (Button) findViewById(R.id.card3);
+                @SuppressLint({"MissingInflatedId", "LocalSuppress"}) Button c4 = (Button) findViewById(R.id.card4);
+
+
+                try_again.setOnClickListener(View -> noMatch());
+
+
+
+                c1.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        c1.setText(gameWords[0]);
+
+                        bttn_List.add(c1);
+
+                        checkMatch(c1);
+
+                    }
+                });
+
+                c2.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        c2.setText(gameWords[3]);
+                        bttn_List.add(c2);
+                        checkMatch(c2);
+
+                    }
+                });
+
+                c3.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        c3.setText(gameWords[0]);
+                        bttn_List.add(c3);
+                        checkMatch(c3);
+                    }
+                });
+
+                c4.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        c4.setText(gameWords[3]);
+
+                        bttn_List.add(c4);
+
+                        checkMatch(c4);
+                    }
+                });
+
+
+
+
+break;
+            case 109:
+                break;
+            default:
+                break;
 
         }
 
 
 
+
+
     }
+
+    private void checkMatch(Button button){
+
+
+        String text = (String) button.getText();
+        if(open_List.contains(text) && bttn_List.size() > 1){
+            score+=2;
+            score_box.setText("Score: "+ score);
+            bttn_List.clear();
+            open_List.clear();
+        }else if (bttn_List.size() > 1){
+
+            for(int i = 0; i < bttn_List.size(); i++){
+                rmv_List.add(bttn_List.get(i));
+            }
+
+            if(score != 0) {
+                score -= 1;
+                score_box.setText("Score: " + score);
+
+            }
+            bttn_List.clear();
+            open_List.clear();
+            try_again.setVisibility(View.VISIBLE);
+
+        }else{
+            open_List.add(text);
+        }
+
+    }
+
+    private void noMatch(){
+        if(!rmv_List.isEmpty()){
+            for(int i = 0; i < rmv_List.size(); i++){
+                rmv_List.get(i).setText("");
+            }
+            try_again.setVisibility(View.INVISIBLE);
+        }
+
+
+    }
+
+
+
 }
