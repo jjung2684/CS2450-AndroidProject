809c0d55e8d2301c24b106e97b30a2752c8dc3b0
spencerCPP <srbarrett@cpp.edu>
2022-12-13 17:28:33 -0800

    Fixed sound issues when rotating...implemented binder for service

--- app/src/main/java/com/android/cs2450_androidproject/BackgroundSoundService.java
+++ app/src/main/java/com/android/cs2450_androidproject/BackgroundSoundService.java
@@ -2,41 +2,89 @@ package com.android.cs2450_androidproject;
 
 import android.app.Service;
 import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
 import android.media.MediaPlayer;
+import android.os.Binder;
 import android.os.IBinder;
 import android.widget.Toast;
+
 import androidx.annotation.Nullable;
-public class BackgroundSoundService extends Service {
-    MediaPlayer mediaPlayer;
+
+import java.io.IOException;
+
+public class BackgroundSoundService extends Service implements MediaPlayer.OnPreparedListener {
+    private MediaPlayer mediaPlayer;
 
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        return new LocalBinder();
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
-        mediaPlayer = MediaPlayer.create(this, R.raw.jazz_music);
-        mediaPlayer.setLooping(true); // Set looping
-        mediaPlayer.setVolume(100, 100);
     }
 
+    /**
+     * Helper class for binding service to components
+     */
+    public class LocalBinder extends Binder {
+        BackgroundSoundService getService() {
+            return BackgroundSoundService.this;
+        }
+    }
+
+    /**
+     * @param intent  - intent passed from GameActivity (music service)
+     * @param flags   - (Unused)
+     * @param startId - (Unused)
+     * @return START_STICKY - tells the OS to recreate the service after it has enough memory
+     */
     public int onStartCommand(Intent intent, int flags, int startId) {
-        mediaPlayer.start();
-        Toast.makeText(getApplicationContext(), "Playing Background Music",    Toast.LENGTH_SHORT).show();
-        return startId;
+
+        /**
+         * Initialize a MediaPlayer object and pass the Raw (Jazz_Music) as resource
+         */
+        mediaPlayer = new MediaPlayer();
+        AssetFileDescriptor afd = getResources().openRawResourceFd(R.raw.jazz_music);
+        try {
+            mediaPlayer.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            afd.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        mediaPlayer.setLooping(true); // Set looping
+        mediaPlayer.setVolume(100, 100);
+        Toast.makeText(getApplicationContext(), "Playing Background Music", Toast.LENGTH_SHORT).show();
+        mediaPlayer.setOnPreparedListener(this);
+        mediaPlayer.prepareAsync();
+
+        // When MediaPlayer is "finished" release object
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.release();
+            }
+        });
+        return START_STICKY;
     }
 
     @Override
     public void onDestroy() {
-
+        stopSelf();     // Stop service when destroyed
         mediaPlayer.stop();
-        mediaPlayer.release();
     }
+
 
     @Override
-    public void onLowMemory() {
+    public void onPrepared(MediaPlayer mp) {
+        mp.start();
     }
+
 }
