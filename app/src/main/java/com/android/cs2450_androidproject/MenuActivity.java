65b90b1f8ff44b6206de5b411bbdaea2566c580e
spencerCPP <srbarrett@cpp.edu>
2022-12-13 22:35:52 -0800

    Added button to switch to scores activity

--- app/src/main/java/com/android/cs2450_androidproject/MenuActivity.java
+++ app/src/main/java/com/android/cs2450_androidproject/MenuActivity.java
@@ -6,13 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
-/**
- * Author:
- * Spencer Barrett
- */
 
 
 
@@ -20,25 +14,34 @@ import android.widget.Button;
  * The purpose of this class is to serve as a menu screen for the
  * application. When start_bttn is clicked, the activity changes
  * from MenuActivity (this) -> LevelsActivity.
+ * @author Spencer Barrett
  */
 public class MenuActivity extends AppCompatActivity {
     // Declare variables
-    Button start_bttn;
+    Button start_button, scores_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-        start_bttn = (Button) findViewById(start_activity_bttn);
-        start_bttn.setOnClickListener(View -> startGame());
+
+        start_button = (Button) findViewById(start_activity_bttn);
+        scores_button = (Button) findViewById(R.id.scores_button);
+
+        start_button.setOnClickListener(View -> startGame());
+        scores_button.setOnClickListener(View -> goToScores());
 
     }
 
     /**
-     * Helper method to change activities on start_bttn click.
+     * Helper method to change activities on start_button click.
      */
     private void startGame(){
         Intent myIntent = new Intent(MenuActivity.this, LevelsActivity.class);
+        startActivity(myIntent);
+    }
+    private void goToScores(){
+        Intent myIntent = new Intent(MenuActivity.this, ScoresActivity.class);
         startActivity(myIntent);
     }
 
